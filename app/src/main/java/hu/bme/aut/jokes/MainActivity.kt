4453db553958fa47d48f9d1177048b84c0791f05
Marogna Peter <prenzo97@gmail.com>
2021-04-06 15:29:49 +0200

    Handle navigation

--- app/src/main/java/hu/bme/aut/jokes/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/jokes/MainActivity.kt
@@ -2,13 +2,53 @@ package hu.bme.aut.jokes
 
 import android.os.Bundle
 import co.zsmb.rainbowcake.navigation.SimpleNavActivity
+import hu.bme.aut.jokes.ui.about.AboutFragment
+import hu.bme.aut.jokes.ui.likedjokes.LikedJokesFragment
+import hu.bme.aut.jokes.ui.myjokes.MyJokesFragment
+import hu.bme.aut.jokes.ui.randomjokes.RandomJokesFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : SimpleNavActivity(), ToolbarHandler {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setupBottomNav()
 
+        if (savedInstanceState == null) {
+            navigator.add(RandomJokesFragment())
+        }
+    }
+
+    private fun setupBottomNav() {
+        bottomNav.setOnNavigationItemReselectedListener {
+            when (it.itemId) {
+                R.id.randomJokes -> {
+                    navigator.replace(RandomJokesFragment())
+                }
+                R.id.likedJokes -> {
+                    navigator.replace(LikedJokesFragment())
+                }
+                R.id.myJokes -> {
+                    navigator.replace(MyJokesFragment())
+                }
+            }
+        }
+    }
+
+    private fun setupToolbar() {
+        toolbar.inflateMenu(R.menu.toolbar_menu)
+        toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.about -> {
+                    navigator.add(AboutFragment())
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
     }
 
     override fun setTitle(title: String) {
