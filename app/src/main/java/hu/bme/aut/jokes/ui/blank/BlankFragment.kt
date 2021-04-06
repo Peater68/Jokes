2c118cd5eaea36666f4568e98589dad7e9fd8f31
Marogna Peter <prenzo97@gmail.com>
2021-04-06 12:27:55 +0200

    Implement ViewBindingFragment in BlankFragment

--- app/src/main/java/hu/bme/aut/jokes/ui/blank/BlankFragment.kt
+++ app/src/main/java/hu/bme/aut/jokes/ui/blank/BlankFragment.kt
@@ -1,15 +1,20 @@
 package hu.bme.aut.jokes.ui.blank
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import co.zsmb.rainbowcake.base.RainbowCakeFragment
+import android.view.ViewGroup
 import co.zsmb.rainbowcake.dagger.getViewModelFromFactory
-import hu.bme.aut.jokes.R
+import hu.bme.aut.jokes.databinding.FragmentBlankBinding
+import hu.bme.aut.jokes.ui.common.ViewBindingFragment
 
-class BlankFragment : RainbowCakeFragment<BlankViewState, BlankViewModel>() {
+class BlankFragment : ViewBindingFragment<FragmentBlankBinding, BlankViewState, BlankViewModel>() {
 
     override fun provideViewModel() = getViewModelFromFactory()
-    override fun getViewResource() = R.layout.fragment_blank
+    override fun initViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = FragmentBlankBinding.inflate(inflater, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -26,5 +31,4 @@ class BlankFragment : RainbowCakeFragment<BlankViewState, BlankViewModel>() {
     override fun render(viewState: BlankViewState) {
         // TODO Render state
     }
-
 }
