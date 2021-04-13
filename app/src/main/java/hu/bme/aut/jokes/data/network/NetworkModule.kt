943db04295a264e4b787aabce72a4e7e24b31500
Marogna Peter <prenzo97@gmail.com>
2021-04-13 20:58:09 +0200

    Add NetworkModule class

---
+++ app/src/main/java/hu/bme/aut/jokes/data/network/NetworkModule.kt
@@ -0,0 +1,42 @@
+package hu.bme.aut.jokes.data.network
+
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+abstract class NetworkModule {
+
+    companion object {
+        private const val BASE_URL = "https://v2.jokeapi.dev"
+    }
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi.Builder().build()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addNetworkInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BASIC
+            })
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+}
