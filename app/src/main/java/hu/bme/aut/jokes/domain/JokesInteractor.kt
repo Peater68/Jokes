d96a105a9261a5a5a28a1a2db418185d1f1d05d5
Marogna Peter <prenzo97@gmail.com>
2021-04-13 21:51:05 +0200

    Add NetworkDataSource to JokesInteractor

--- app/src/main/java/hu/bme/aut/jokes/domain/JokesInteractor.kt
+++ app/src/main/java/hu/bme/aut/jokes/domain/JokesInteractor.kt
@@ -1,7 +1,21 @@
 package hu.bme.aut.jokes.domain
 
+import hu.bme.aut.jokes.data.network.NetworkDataSource
+import hu.bme.aut.jokes.domain.model.DomainJoke
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class JokesInteractor @Inject constructor()
+class JokesInteractor @Inject constructor(
+    private val networkDataSource: NetworkDataSource
+) {
+    suspend fun getCategories(): List<String> {
+        return networkDataSource.getCategories()
+    }
+
+    suspend fun getRandomJokes(): List<DomainJoke> {
+        return networkDataSource.getRandomJokes()
+    }
+
+    // TODO: add remaining calls
+}
