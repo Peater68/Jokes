b298e709adaf59381df649e9fb55c8342de9db7c
Marogna Peter <prenzo97@gmail.com>
2021-04-13 21:48:59 +0200

    Add NetworkDataSource with real API calls

--- app/src/main/java/hu/bme/aut/jokes/domain/model/DomainJoke.kt
+++ app/src/main/java/hu/bme/aut/jokes/domain/model/DomainJoke.kt
@@ -1,5 +1,7 @@
 package hu.bme.aut.jokes.domain.model
 
+import hu.bme.aut.jokes.data.network.model.JokeDto
+
 sealed class DomainJoke {
     abstract val category: String
     abstract val flags: List<Flag>
@@ -23,3 +25,23 @@ data class DomainTwoPartJoke(
     override val id: Long,
     override val safe: Boolean,
 ) : DomainJoke()
+
+fun JokeDto.toDomainJoke(): DomainJoke {
+    return when (type) {
+        JokeDto.Type.SINGLE -> DomainSingleJoke(
+            id = id,
+            safe = safe,
+            category = category,
+            flags = flags.toDomainFlagList(),
+            joke = joke!!
+        )
+        JokeDto.Type.TWOPART -> DomainTwoPartJoke(
+            id = id,
+            safe = safe,
+            category = category,
+            flags = flags.toDomainFlagList(),
+            setup = setup!!,
+            delivery = delivery!!
+        )
+    }
+}
--- app/src/main/java/hu/bme/aut/jokes/domain/model/Flag.kt
+++ app/src/main/java/hu/bme/aut/jokes/domain/model/Flag.kt
@@ -1,5 +1,7 @@
 package hu.bme.aut.jokes.domain.model
 
+import hu.bme.aut.jokes.data.network.model.FlagsDto
+
 enum class Flag {
     NSFW,
     RELIGIOUS,
@@ -7,4 +9,15 @@ enum class Flag {
     RACIST,
     SEXIST,
     EXPLICIT,
+}
+
+fun FlagsDto.toDomainFlagList(): List<Flag> {
+    return mutableListOf<Flag>().apply {
+        if (nsfw) add(Flag.NSFW)
+        if (religious) add(Flag.RELIGIOUS)
+        if (political) add(Flag.POLITICAL)
+        if (racist) add(Flag.RACIST)
+        if (sexist) add(Flag.SEXIST)
+        if (explicit) add(Flag.EXPLICIT)
+    }
 }
