a816c037daeb82545f22581c96e244833fad48d3
Marogna Peter <prenzo97@gmail.com>
2021-04-14 17:51:57 +0200

    Add Room calls to JokesInteractor

--- app/src/main/java/hu/bme/aut/jokes/domain/JokesInteractor.kt
+++ app/src/main/java/hu/bme/aut/jokes/domain/JokesInteractor.kt
@@ -1,5 +1,6 @@
 package hu.bme.aut.jokes.domain
 
+import hu.bme.aut.jokes.data.disk.DiskDataSource
 import hu.bme.aut.jokes.data.network.NetworkDataSource
 import hu.bme.aut.jokes.domain.model.DomainJoke
 import javax.inject.Inject
@@ -7,7 +8,8 @@ import javax.inject.Singleton
 
 @Singleton
 class JokesInteractor @Inject constructor(
-    private val networkDataSource: NetworkDataSource
+    private val networkDataSource: NetworkDataSource,
+    private val diskDataSource: DiskDataSource
 ) {
     suspend fun getCategories(): List<String> {
         return networkDataSource.getCategories()
@@ -17,5 +19,23 @@ class JokesInteractor @Inject constructor(
         return networkDataSource.getJokesByCategories(categories)
     }
 
-    // TODO: add remaining calls
+    suspend fun saveLikedJoke(joke: DomainJoke) {
+        diskDataSource.saveLikedJoke(joke)
+    }
+
+    suspend fun saveMyJoke(joke: DomainJoke) {
+        diskDataSource.saveMyJoke(joke)
+    }
+
+    suspend fun getLikedJokes() = diskDataSource.getLikedJokes()
+
+    suspend fun getMyJokes() = diskDataSource.getMyJokes()
+
+    suspend fun deleteJokeById(jokeId: Long) {
+        diskDataSource.deleteJokeById(jokeId)
+    }
+
+    suspend fun deleteAllJokes() {
+        diskDataSource.deleteAllJokes()
+    }
 }
