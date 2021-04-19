08e77fa19d6cf92e7b39002576174952860883f2
Marogna Péter <prenzo97@gmail.com>
2021-04-19 23:39:13 +0200

    Add MockNetworkModule

---
+++ app/src/test/java/hu/bme/aut/jokes/mock/MockNetworkModule.kt
@@ -0,0 +1,29 @@
+package hu.bme.aut.jokes.mock
+
+import dagger.Module
+import dagger.Provides
+import hu.bme.aut.jokes.data.network.api.FakeApi
+import hu.bme.aut.jokes.data.network.api.RealApi
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
+
+@Module
+class MockNetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFakeApi(): FakeApi = MockFakeApi()
+
+    @Provides
+    @Singleton
+    fun provideRealApi(): RealApi = MockRealApi()
+}
