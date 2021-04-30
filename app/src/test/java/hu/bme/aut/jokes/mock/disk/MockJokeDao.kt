b8b9bcdf4ba181772f13ea9525df632f82e06dc6
Marogna Péter <prenzo97@gmail.com>
2021-04-30 22:23:02 +0200

    Mock JokeDao

--- app/src/test/java/hu/bme/aut/jokes/mock/disk/MockJokeDao.kt
+++ app/src/test/java/hu/bme/aut/jokes/mock/disk/MockJokeDao.kt
@@ -3,21 +3,42 @@ package hu.bme.aut.jokes.mock.disk
 import hu.bme.aut.jokes.data.disk.dao.JokeDao
 import hu.bme.aut.jokes.data.disk.model.RoomJoke
 import hu.bme.aut.jokes.data.disk.model.RoomJokeMode
+import kotlin.random.Random
 
 class MockJokeDao : JokeDao {
+    companion object {
+        var jokes: MutableList<RoomJoke> = mutableListOf()
+    }
+
     override suspend fun insertJoke(joke: RoomJoke): Long {
-        TODO("Not yet implemented")
+        val randomId = Random.nextLong()
+        jokes.add(joke.copy(id = randomId))
+        return randomId
+    }
+
+    override suspend fun getJokeById(id: Long): RoomJoke? {
+        return jokes.firstOrNull { it.id == id }
     }
 
     override suspend fun getJokesByMode(mode: RoomJokeMode): List<RoomJoke> {
-        TODO("Not yet implemented")
+        return jokes.filter { it.mode == mode }
     }
 
     override suspend fun deleteJokeById(id: Long) {
-        TODO("Not yet implemented")
+        jokes.removeIf { it.id == id }
     }
 
     override suspend fun deleteAllJokes() {
-        TODO("Not yet implemented")
+        jokes.clear()
+    }
+
+    override suspend fun deleteJokesByMode(mode: RoomJokeMode) {
+        jokes.removeIf { it.mode == mode }
+    }
+
+    override suspend fun updateJokeMode(id: Long, mode: RoomJokeMode) {
+        val index = jokes.indexOfFirst { it.id == id }
+        if (index == -1) return
+        jokes[index] = jokes[index].copy(mode = mode)
     }
 }
