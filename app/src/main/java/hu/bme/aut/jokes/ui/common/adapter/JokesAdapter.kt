25a04f5e7eea87a9e86459ebe603d00ce47febd3
Marogna Péter <prenzo97@gmail.com>
2021-04-30 14:03:04 +0200

    Add type to JokesAdapter

--- app/src/main/java/hu/bme/aut/jokes/ui/common/adapter/JokesAdapter.kt
+++ app/src/main/java/hu/bme/aut/jokes/ui/common/adapter/JokesAdapter.kt
@@ -11,9 +11,13 @@ import hu.bme.aut.jokes.databinding.RowJokeItemBinding
 import hu.bme.aut.jokes.ui.common.model.Joke
 import hu.bme.aut.jokes.ui.common.model.JokeComparator
 
+enum class JokesAdapterType {
+    RANDOM, MY
+}
 
 class JokesAdapter(
     private val context: Context,
+    private val type: JokesAdapterType = JokesAdapterType.RANDOM,
     private val iconClick: ((joke: Joke) -> Unit)? = null
 ) :
     ListAdapter<Joke, JokesAdapter.ViewHolder>(JokeComparator) {
@@ -27,18 +31,30 @@ class JokesAdapter(
         with(holder.binding) {
             categoryText.text = item.category
             jokeText.text = item.joke
-            if (item.isLiked) {
-                likeButton.setImageResource(R.drawable.ic_star_filled)
-            } else {
-                likeButton.setImageResource(R.drawable.ic_star_outline)
-            }
-            likeButton.setOnClickListener { iconClick?.invoke(item) }
+
+            setupIcon(item)
 
             flagChipGroup.removeAllViews()
             item.flags.forEach {
                 flagChipGroup.addView(Chip(context).apply { text = it.name })
             }
         }
+    }
+
+    private fun RowJokeItemBinding.setupIcon(joke: Joke) {
+        when (type) {
+            JokesAdapterType.RANDOM -> {
+                if (joke.isLiked) {
+                    likeButton.setImageResource(R.drawable.ic_star_filled)
+                } else {
+                    likeButton.setImageResource(R.drawable.ic_star_outline)
+                }
+            }
+            JokesAdapterType.MY -> {
+                likeButton.setImageResource(R.drawable.ic_delete)
+            }
+        }
+        likeButton.setOnClickListener { iconClick?.invoke(joke) }
     }
 
     inner class ViewHolder(val binding: RowJokeItemBinding) : RecyclerView.ViewHolder(binding.root)
