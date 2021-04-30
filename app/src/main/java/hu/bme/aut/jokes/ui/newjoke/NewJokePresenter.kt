e6f02ba4f29af2c037bced107c145c98be54a79d
Marogna Péter <prenzo97@gmail.com>
2021-04-30 12:41:52 +0200

    Add functionality to NewJoke screen

--- app/src/main/java/hu/bme/aut/jokes/ui/newjoke/NewJokePresenter.kt
+++ app/src/main/java/hu/bme/aut/jokes/ui/newjoke/NewJokePresenter.kt
@@ -2,14 +2,34 @@ package hu.bme.aut.jokes.ui.newjoke
 
 import co.zsmb.rainbowcake.withIOContext
 import hu.bme.aut.jokes.domain.JokesInteractor
+import hu.bme.aut.jokes.domain.model.DomainSingleJoke
+import hu.bme.aut.jokes.domain.model.Flag
+import java.util.*
 import javax.inject.Inject
 
 class NewJokePresenter @Inject constructor(
     private val jokesInteractor: JokesInteractor
 ) {
 
-    suspend fun getData(): String = withIOContext {
-        ""
+    suspend fun getCategories() = withIOContext {
+        jokesInteractor.getCategories().filter { it.toLowerCase(Locale.getDefault()) != "any" }
+    }
+
+    suspend fun submitJoke(
+        category: String,
+        joke: String,
+        safe: Boolean,
+        flags: List<Flag>
+    ) = withIOContext {
+        jokesInteractor.saveMyJoke(
+            DomainSingleJoke(
+                id = 0L,
+                category = category,
+                joke = joke,
+                safe = safe,
+                flags = flags,
+            )
+        )
     }
 
 }
