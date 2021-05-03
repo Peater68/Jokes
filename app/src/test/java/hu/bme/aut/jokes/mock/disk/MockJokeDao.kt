47bd5df52ef674c7910164cf373eab71bd03bd10
Marogna Péter <prenzo97@gmail.com>
2021-05-03 13:52:00 +0200

    Fix joke id when saving

--- app/src/test/java/hu/bme/aut/jokes/mock/disk/MockJokeDao.kt
+++ app/src/test/java/hu/bme/aut/jokes/mock/disk/MockJokeDao.kt
@@ -3,17 +3,47 @@ package hu.bme.aut.jokes.mock.disk
 import hu.bme.aut.jokes.data.disk.dao.JokeDao
 import hu.bme.aut.jokes.data.disk.model.RoomJoke
 import hu.bme.aut.jokes.data.disk.model.RoomJokeMode
-import kotlin.random.Random
+import hu.bme.aut.jokes.data.disk.model.RoomJokeType
 
 class MockJokeDao : JokeDao {
     companion object {
-        var jokes: MutableList<RoomJoke> = mutableListOf()
+        const val EXAMPLE_LIKED_JOKE_ID = 20L
+        const val EXAMPLE_MY_JOKE_ID = 21L
+        const val INVALID_JOKE_ID = -123L
+
+        var jokes: MutableList<RoomJoke> = mutableListOf(
+            RoomJoke(
+                id = EXAMPLE_LIKED_JOKE_ID,
+                mode = RoomJokeMode.LIKED,
+                category = "Dark",
+                flags = emptyList(),
+                safe = true,
+                type = RoomJokeType.SINGLE,
+                joke = "joke",
+                setup = null,
+                delivery = null,
+            ),
+            RoomJoke(
+                id = EXAMPLE_MY_JOKE_ID,
+                mode = RoomJokeMode.MY,
+                category = "Misc",
+                flags = emptyList(),
+                safe = true,
+                type = RoomJokeType.SINGLE,
+                joke = "joke",
+                setup = null,
+                delivery = null,
+            )
+        )
     }
 
     override suspend fun insertJoke(joke: RoomJoke): Long {
-        val randomId = Random.nextLong()
-        jokes.add(joke.copy(id = randomId))
-        return randomId
+        val index = jokes.indexOfFirst { it.id == joke.id }
+        if (index != -1) {
+            jokes.removeAt(index)
+        }
+        jokes.add(joke)
+        return joke.id
     }
 
     override suspend fun getJokeById(id: Long): RoomJoke? {
